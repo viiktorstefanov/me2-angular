a2f769721564304e3e915ca0d3437e6ae8348fe4
Viktor Stefanov <victorystefanov@gmail.com>
2024-01-30 17:12:06 +0200

    validation register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -11,8 +11,8 @@ export class RegisterComponent {
 
   form = this.fb.group({
     firstName: ['', [Validators.required, Validators.minLength(3)] ],
-    lastName: ['', [Validators.required, Validators.minLength(5)] ],
-    email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)] ],
+    email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/), Validators.minLength(9)]],
     password: ['', [Validators.required, Validators.minLength(8)] ],
     phoneNumber: ['', [Validators.required, Validators.minLength(10)] ],
   })
@@ -24,5 +24,25 @@ export class RegisterComponent {
 
   submitHandler() :void {
     console.log(this.form.value);
+  }
+
+  get email() {
+    return this.form.controls['email'];
+  }
+
+  get firstName() {
+    return this.form.controls['firstName'];
+  }
+
+  get lastName() {
+    return this.form.controls['lastName'];
+  }
+
+  get phoneNumber() {
+    return this.form.controls['phoneNumber'];
+  }
+
+  get password() {
+    return this.form.controls['password'];
   }
 }
