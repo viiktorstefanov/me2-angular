3da7b9c67c140df254611c077bc5dcd79e17f1db
Viktor Stefanov <victorystefanov@gmail.com>
2024-02-04 22:06:21 +0200

    deploy req

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiUrl: 'http://localhost:5000',
+    USER_KEY: 'userData',
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    apiUrl: 'http://localhost:5000', // domain -> will not be localhost in prod
+    USER_KEY: 'userData',
+};
