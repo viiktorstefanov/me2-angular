e6be0f3cf00d450ed23e0fb4305a16846c95df1e
Viktor Stefanov <victorystefanov@gmail.com>
2024-02-06 14:43:24 +0200

    server route for places fixed

--- server/controllers/placesController.js
+++ server/controllers/placesController.js
@@ -8,18 +8,35 @@ placesController.get('/all', async (req, res) => {
     console.log('All places were sent.');
 });
 
+placesController.get('/recent', async (req, res) => {
+    const recentPlaces = await getRecentPlaces();
+    res.json(recentPlaces);
+    console.log('Recent places were sent.');
+});
+
+placesController.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const place = await getById(id);   
+        res.json(place).end();
+            console.log('single place send')
+    } catch (error) {
+        const message = parseError(error);
+        res.status(400).json({ message });
+    }
+});
+
 placesController.post('/add', async (req, res) => {
     try {
-            // const user = JSON.parse(req.headers.user);
+            const user = JSON.parse(req.headers.user);  
             const place = req.body;
             if(!req.body || req.body.length <= 0) {
                 throw new Error('Please, enter your new place');
             };
-            // const newPlace = await addPlace(place, user._id);
-            const newPlace = await addPlace(place, '65b80610f0ed0396dffaab72');
+            const newPlace = await addPlace(place, user._id);
             console.log('new place added');
-            res.json(newPlace).end();
-        
+            res.status(204).end();
+
     } catch (error) {
         const message = parseError(error);
         res.status(400).json({ message });
@@ -46,26 +63,18 @@ placesController.delete('/:id', async (req, res) => {
 
 placesController.put('/:id', async (req, res) => {
     try {
-        const user = JSON.parse(req.headers.user);
         const placeId = req.params.id;
         if(!req.body || req.body.length <= 0) {
             throw new Error('Missing information about place');
         };
-        const editedPlace = req.body;
-        
+        const editedPlace = req.body;  
         const updatedPlace = await editPlace(placeId, editedPlace);
-        res.json(updatedPlace).end();
+        res.status(204).end();
         console.log(`place edited`);
     } catch (error) {
         const message = parseError(error);
         res.status(400).json({ message });
     };
-});
-
-placesController.get('/recent', async (req, res) => {
-    const recentPlaces = await getRecentPlaces();
-    res.json(recentPlaces);
-    console.log('Recent places were sent.');
 });
 
 module.exports = placesController;
